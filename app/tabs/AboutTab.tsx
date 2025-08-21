7b72d369c819900042a8c91458fbdd086698641e
Duc Dang <dangminhd3@gmail.com>
2025-08-22 01:56:47 +1000

    Update aboutTab

---
+++ app/tabs/AboutTab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutTab(): JSX.Element {
+  return (
+    <div id="about" className="text-[--foreground]">
+      <p>About content</p>
+    </div>
+  );
+}
+
+
